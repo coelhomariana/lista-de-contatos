02bfe0beb501f4bfdbc866fc63c2ad7d7a177ebe
Mariana Coelho <coelhomariana@outlook.com>
2020-07-06 21:49:01 -0300

    API retornando corretamente uma lista de pessoas

--- App.js
+++ App.js
@@ -2,12 +2,41 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
+import axios from "axios";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then((response) => {
+        const { results } = response.data;
+        this.setState({
+          people: results,
+        });
+      });
+  }
+
+  renderList() {
+    const textElements = this.state.people.map((people) => {
+      const { first } = people.name;
+      return <Text key={first}>{first}</Text>;
+    });
+    return textElements;
+  }
+
   render() {
     return (
       <View>
-        <Header title="People"></Header>
+        <Header title="Pessoas"></Header>
+        {this.renderList()}
       </View>
     );
   }
